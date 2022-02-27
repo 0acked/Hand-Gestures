ffbd5e9948da2d8781012bfe3972dea592bc0b78
0acked <98188838+0acked@users.noreply.github.com>
2022-02-27 10:18:26 +0000

    Add files via upload

--- m.js
+++ m.js
@@ -20,3 +20,50 @@ classifier=ImageClassifier("https://teachablemachine.withgoogle.com/models/9-xW-
 function modelLoaded(){
     console.log("Model loaded!");
 }
+function takePred(){
+    img= document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(results,error){
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        prediction= results[0].label;
+        speak();
+
+        if(results[0].label=="Thumbs up!")
+        {
+            document.getElementById("predict").innerHTML="prediction-<i>&#128078;</i>";
+        }
+        if(results[0].label=="Thumbs down...")
+        {
+            document.getElementById("predict").innerHTML="prediction-<i>&#128077;</i>";
+        }
+        if(results[0].label=="Hi!")
+        {
+            document.getElementById("predict").innerHTML="prediction-<i>&#9995;</i>";
+        }
+        if(results[0].label=="Perfect!")
+        {
+            document.getElementById("predict").innerHTML="prediction-<i>&#128076;</i>";
+        }
+        if(results[0].label=="Punk!")
+        {
+            document.getElementById("predict").innerHTML="prediction-<i>&#129304;</i>";
+        }
+        if(results[0].label=="Point!")
+        {
+            document.getElementById("predict").innerHTML="prediction-<i>&#9757;</i>";
+        }
+    }
+}
+function speak(){
+    synth= window.speechSynthesis;
+    speakdata1="The first prediction is"+prediction;
+    var utterThis=new SpeechSynthesisUtterance(speakdata1);
+    synth.speak(utterThis);
+}
